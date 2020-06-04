97eca16ca66b626641e9a560a16fe274963db906
Your Name <you@example.com>
2020-06-04 14:11:02 +0200

    Add Category entity

---
+++ src/main/java/com/alezmu/recipie/domain/Difficulty.java
@@ -0,0 +1,6 @@
+package com.alezmu.recipie.domain;
+
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/alezmu/recipie/domain/Recipe.java
+++ src/main/java/com/alezmu/recipie/domain/Recipe.java
@@ -18,11 +18,16 @@ public class Recipe {
     private String directions;
     @Lob
     private Byte[] image;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
-
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+    joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -110,5 +115,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
